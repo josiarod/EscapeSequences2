19ebd59dc7299ea77aa37c09f5d6c144d540054f
GBTC441011UR <GBTC441011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-01-27 10:42:49 -0500

    escape sequences

---
+++ src/EscapeSequences.java
@@ -0,0 +1,10 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("Cost\t Quantity\t Total");
+        System.out.println("$1,000.00\t 4\t\t $4,000.00");
+        System.out.println("$  50.00\t 8\t\t $400.00");
+
+        System.out.println("_______________________________");
+        System.out.println("TOTAL:\t\t 12\t\t $4,400.00");
+    }
+}
